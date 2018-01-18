93b5e4e94d76fafd9782d12ee346d162f3b43693
stefanstojanov <stefan_stojanov@live.com>
2018-01-18 21:13:18 +0100

    sredeno

--- application/views/pocetna.php
+++ application/views/pocetna.php
@@ -4,7 +4,7 @@
         <title>Почетна</title>
     </head>
     <body>
-        <h1>Добредојте <?= $this->session->userdata('korisnicko_ime') ?></h1>
+        <h1>Добредојде <?= $this->session->userdata('korisnicko_ime') ?></h1>
         <a href="<?= site_url('pocetna/odjavi_se') ?>">Одјави се</a>
     </body>
 </html>
