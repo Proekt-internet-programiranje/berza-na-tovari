0254cb2435c0b1b7f3c2612d324a7928964d57a4
AndrejZdravev <32909993+AndrejZdravev@users.noreply.github.com>
2018-01-28 20:37:28 +0100

    spedicija php

--- application/controllers/spedicija.php
+++ application/controllers/spedicija.php
@@ -6,9 +6,36 @@ class Spedicija extends CI_Controller{
         parent::__construct();
     }
     
-    public function index(){
-        $this->load->view('header');
-        $this->load->view('spedicija');
-        $this->load->view('footer');
+    public function index()
+	{
+        //$this->load->view('admin');
+        $this->prikazi((object)array('output' => '' , 'js_files' => array() , 'css_files' => array()));
+	}
+    
+    public function prikazi($output = null)
+	{
+        $this->load->view('spedicija',(array)$output);
+	}
+
+    
+    public function vnesi_tovar()
+    {
+        $tabela = new grocery_CRUD();
+       
+        $tabela->set_table('tovar');
+        $tabela->set_relation('id_kompanija','kompanija','imekompanija');
+        $tabela->set_relation('tip_na_potrebno_vozilo','tip_na_vozilo','naziv');
+        $tabela->set_theme('datatables');
+        $tabela->set_language('makedonski');
+        $tabela->columns('id_kompanija','utovarno_mesto','istovarno_mesto','tezina','cena','tip_na_potrebno_vozilo');
+        $tabela->display_as('id_kompanija','Назив на компанија');
+        $tabela->display_as('tip_na_vozilo','Тип на возило');
+        $tabela->display_as('utovarno_mesto','Место на утовар');
+        $tabela->display_as('istovarno_mesto','Место на истовар');
+        $tabela->display_as('tezina','Тежина');
+        $tabela->display_as('cena','Цена');
+        $tabela->display_as('tip_na_potrebno_vozilo','Потребно возило');
+        $tabela->where('imekompanija',($this->session->userdata('imekompanija')));
+        $this->prikazi($tabela->render());
     }
 }
--- application/views/spedicija.php
+++ application/views/spedicija.php
@@ -1,11 +1,15 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <!DOCTYPE html>
+<?php $this->view('header'); ?>
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <ul class="nav navbar-nav">
-      <li><a href="<?= site_url()?>/spedicija/vnesi_tovar">Внесување на товар</a></li>
-      <li><a href="<?= site_url()?>/spedicija/pregled_vozila">Преглед на слободни возила</a></li>
-      <li><a href="<?= site_url()?>/spedicija/vnesi_tura">Внесување на тура</a></li>
+    <li><a href="<?= site_url()?>/spedicija/vnesi_tovar">Внесување на товар</a></li>
+    <li><a href="<?= site_url()?>/spedicija/pregled_vozila">Преглед на слободни возила</a></li>
+    <li><a href="<?= site_url()?>/spedicija/vnesi_tura">Внесување на тура</a></li>
     </ul>
   </div>
 </nav>
+<?php 
+echo $output;
+$this->view('footer'); ?>
