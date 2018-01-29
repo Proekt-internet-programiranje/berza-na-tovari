46a791a5b8ef1fad9c5a2d44d37ec419c6f6150d
kozha <kozhinkov.f@gmail.com>
2018-01-30 00:11:43 +0100

    push

--- application/views/header.php
+++ application/views/header.php
@@ -4,6 +4,20 @@
   <meta charset="UTF-8">
   <title>Берза на товари</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+<title>Берза на товари</title>
+<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+<!-- Bootstrap 3.3.2 -->
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />    
+<!-- FontAwesome 4.3.0 -->
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+<!-- Ionicons 2.0.0 -->
+<link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />    
+<!-- Theme style -->
+<link href="dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+<!-- AdminLTE Skins. Choose a skin from the css/skins 
+     folder instead of downloading all of them to reduce the load. -->
+<link href="dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
     <?php if(isset($css_files)){
                 foreach($css_files as $file)
 	            echo "<link type='text/css' rel='stylesheet' href='$file' />\n";
@@ -15,17 +29,17 @@
             }
         ?>
 </head>
-<body style="height:100%">
-   <header>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="<?= site_url()?>">Берза на товари</a>
-            </div>
-            <ul class="nav navbar-nav navbar-right">
-            <li class="active"><a href="#">Дома</a></li>
-            <li class="float-right"><a href="<?= site_url()?>/pocetna/odjavi_se">Одјави се</a></li>
-            </ul>
-        </div>
-    </nav>
-   </header>
+<body>
+<div class="w3-on-top">
+  <div class="w3-bar w3-grey  w3-padding w3-card">
+  <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large w3-theme-l1" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+    <a href="<?= site_url()?>" class="w3-bar-item w3-button"><b>Берза </b>на товари</a>
+    <div class="w3-right w3-hide-small">
+      <a href="#" class="w3-bar-item w3-button">Дома</a>
+      <a href="<?= site_url()?>/pocetna/odjavi_se" class="w3-bar-item w3-button">Одјави се</a>
+    </div>
+  </div>
+</div>
+
+        </body>
+        </html>
--- application/views/spedicija.php
+++ application/views/spedicija.php
@@ -1,15 +1,61 @@
+
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <!DOCTYPE html>
 <?php $this->view('header'); ?>
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <ul class="nav navbar-nav">
-    <li><a href="<?= site_url()?>/spedicija/vnesi_tovar">Внесување на товар</a></li>
-    <li><a href="<?= site_url()?>/spedicija/pregled_vozila">Преглед на слободни возила</a></li>
-    <li><a href="<?= site_url()?>/spedicija/prevoznici">Внесување на тура</a></li>
-    </ul>
-  </div>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+.w3-sidebar {
+  z-index: 3;
+
+}
+</style>
+<body>
+<nav class="w3-sidebar w3-light-gray  w3-card w3-bar-block w3-collapse w3-medium w3-theme-l5 w3-animate-left" id="mySidebar">
+  <a href="javascript:void(0)" onclick="w3_close()" class="w3-right w3-xlarge w3-padding-large w3-hover-black w3-hide-large" title="Close Menu">
+    <i class="fa fa-remove"></i>
+  </a>
+ 
+  <a class="w3-bar-item w3-button" href="<?= site_url()?>/spedicija/vnesi_tovar">Внесување на товар</a>
+  <a class="w3-bar-item w3-button" href="<?= site_url()?>/spedicija/pregled_vozila">Преглед на слободни возила</a>
+  <a class="w3-bar-item w3-button" href="<?= site_url()?>/spedicija/prevoznici">Внесување на тура</a>
 </nav>
-<?php if(isset($output)) 
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<div class="w3-main" style="margin-left:210px">
+  <div class="w3-row w3-padding-10">
+  </div>
+  <script>
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
+
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+        overlayBg.style.display = "none";
+    } else {
+        mySidebar.style.display = 'block';
+        overlayBg.style.display = "block";
+    }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+    overlayBg.style.display = "none";
+}
+</script>
+</body>
+</html>
+<?php 
 echo $output;
 $this->view('footer'); ?>
