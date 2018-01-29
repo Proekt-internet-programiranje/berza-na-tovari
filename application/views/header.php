bac98002d62dbece48fbfa7b37905e554d538ed6
stefanstojanov <stefan_stojanov@live.com>
2018-01-29 23:14:27 +0100

    smenet header
    asdasd

--- application/views/header.php
+++ application/views/header.php
@@ -4,10 +4,6 @@
   <meta charset="UTF-8">
   <title>Берза на товари</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="<?= base_url()?>/pomosno/futer/futer.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <?php if(isset($css_files)){
                 foreach($css_files as $file)
 	            echo "<link type='text/css' rel='stylesheet' href='$file' />\n";
