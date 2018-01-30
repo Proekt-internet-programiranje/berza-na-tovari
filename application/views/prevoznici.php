5b28806aad5c1cbd5c99145c7bbe9865fa80b984
kozha <kozhinkov.f@gmail.com>
2018-01-30 02:52:16 +0100

    push

--- application/views/prevoznici.php
+++ application/views/prevoznici.php
@@ -1,5 +1,9 @@
 <?php $this->load->view('spedicija'); ?>
-    <form action="<?php echo site_url('spedicija/obraboti_prevoznikid'); ?>" method="post">
+<div class="w3-card-4" style="margin-top: 10px">
+    <div class="w3-container w3-light-grey">
+      <h2>Избери превозник за турата</h2>
+    </div>
+    <form style="padding: 16px" action="<?php echo site_url('spedicija/obraboti_prevoznikid'); ?>" method="post">
         <select name="prevoznik">
         <?php
             foreach($prevoznik as $opcii)
@@ -8,6 +12,7 @@
             }
         ?>
             </select>
-        <input type="submit" value="Испрати">
+        <input type="submit" value="Избери">
     </form>
+    </div>
 <?php $this->load->view('footer'); ?>
