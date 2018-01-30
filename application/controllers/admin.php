30451a3143ed7267222ac73bb7f5652e66cc3aec
stefanstojanov <stefan_stojanov@live.com>
2018-01-30 03:45:53 +0100

    mojto e sredeno
    sredeno se

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -78,11 +78,12 @@ class Admin extends CI_Controller{
         $tabela->set_relation('id_kompanija','kompanija','imekompanija');
         $tabela->set_theme('datatables');
         $tabela->set_language('makedonski');
-        $tabela->columns('id_vozac','id_kompanija','ime_vozac','tip_na_vozacka');
+        $tabela->columns('id_vozac','id_kompanija','ime_vozac','tip_na_vozacka','ima_tura');
         $tabela->display_as('id_vozac','Корисничко име');
         $tabela->display_as('id_kompanija','Назив на компанија');
         $tabela->display_as('ime_vozac','Име и презиме');
         $tabela->display_as('tip_na_vozacka','Возачка дозвола');
+        $tabela->display_as('ima_tura','Слободен');
         $this->prikazi($tabela->render());
     }
     
@@ -94,13 +95,14 @@ class Admin extends CI_Controller{
         $tabela->set_relation('tip_na_vozilo','tip_na_vozilo','naziv');
         $tabela->set_theme('datatables');
         $tabela->set_language('makedonski');
-        $tabela->columns('id_kompanija','tip_na_vozilo','euro_standard','broj_na_sasija','tip_na_prikolka','registracija');
+        $tabela->columns('id_kompanija','tip_na_vozilo','euro_standard','broj_na_sasija','tip_na_prikolka','registracija','ima_tura');
         $tabela->display_as('id_kompanija','Назив на компанија');
         $tabela->display_as('tip_na_vozilo','Тип на возило');
         $tabela->display_as('euro_standard','ЕУР стандард');
         $tabela->display_as('broj_na_sasija','Број на шасија');
         $tabela->display_as('tip_na_prikolka','Приколка');
         $tabela->display_as('registracija','Регистарски ознаки');
+        $tabela->display_as('ima_tura','Слободно');
         $tabela->set_subject('Возила');
         $this->prikazi($tabela->render());
     }
@@ -133,12 +135,27 @@ class Admin extends CI_Controller{
         $tabela->set_table('tura');
         $tabela->set_relation('id_prevoznik','kompanija','imekompanija');
         $tabela->set_relation('id_spedicija','kompanija','imekompanija');
-        $tabela->set_relation('id_tovar','tovar','id_tovar');
+        $tabela->set_relation('id_tovar','tovar','id_tovar',array('ima_tura' => 'ne'));
         $tabela->set_relation('id_vozac','vozac','ime_vozac');
         $tabela->set_relation('id_vozilo','vozilo','registracija');
         $tabela->set_theme('datatables');
         $tabela->set_language('makedonski');
-        $tabela->fields('id_prevoznik','id_spedicija','id_tovar','id_vozilo','id_vozac');
+        $tabela->columns('id_prevoznik','id_spedicija','id_tovar','id_vozilo','id_vozac','zavrsena');
+        $tabela->fields('id_prevoznik','id_spedicija','id_tovar','id_vozilo','id_vozac','zavrsena');
+        $tabela->display_as('id_prevoznik','Превозник');
+        $tabela->display_as('id_spedicija','Шпедиција');
+        $tabela->display_as('id_tovar','Шифра на товар');
+        $tabela->display_as('id_vozilo','Возило');
+        $tabela->display_as('id_vozac','Возач');
+        $tabela->display_as('zavrsena','Завршена');
+        $tabela->field_type('zavrsena','ne');
+        $tabela->field_type('zavrsena','hidden','ne');
+        $tabela->callback_after_insert(array($this, 'smeni_tovar'));
         $this->prikazi($tabela->render());
+    }
+    
+    public function smeni_tovar($post_array,$primary_key)
+    {
+        $this->db->query("update tovar set ima_tura='da' where tovar.id_tovar='".$post_array["id_tovar"]."'");
     }
 }
--- application/controllers/spedicija.php
+++ application/controllers/spedicija.php
@@ -30,7 +30,7 @@ class Spedicija extends CI_Controller{
         $tabela->set_theme('datatables');
         $tabela->set_language('makedonski');
         $tabela->columns('id_kompanija','utovarno_mesto','istovarno_mesto','tezina','cena','tip_na_potrebno_vozilo');
-        $tabela->fields('id_kompanija','utovarno_mesto','istovarno_mesto','tezina','cena','tip_na_potrebno_vozilo');
+        $tabela->fields('id_kompanija','utovarno_mesto','istovarno_mesto','tezina','cena','tip_na_potrebno_vozilo','ima_tura');
         $tabela->display_as('id_kompanija','Назив на компанија');
         $tabela->display_as('tip_na_vozilo','Тип на возило');
         $tabela->display_as('utovarno_mesto','Место на утовар');
@@ -39,6 +39,7 @@ class Spedicija extends CI_Controller{
         $tabela->display_as('cena','Цена');
         $tabela->display_as('tip_na_potrebno_vozilo','Потребно возило');
         $tabela->where('imekompanija',($this->session->userdata('imekompanija')));
+        $tabela->field_type('ima_tura','hidden','ne');
         //$tabela->callback_before_insert(array($this,'smeni_id'));
         //$tabela->callback_add_field('id_kompanija', function () { 
             //$ime=$this->session->userdata('imekompanija');
@@ -51,7 +52,7 @@ class Spedicija extends CI_Controller{
         $tabela = new grocery_CRUD();
         $tabela->set_table('vozilo');
         $tabela->set_relation('id_kompanija','kompanija','imekompanija');
-        $tabela->set_relation('tip_na_vozilo','tip_na_vozilo','naziv');
+        $tabela->set_relation('tip_na_vozilo','tip_na_vozilo','naziv',array('ima_tura' => 'ne'));
         $tabela->set_theme('datatables');
         $tabela->set_language('makedonski');
         $tabela->columns('id_kompanija','tip_na_vozilo','euro_standard','broj_na_sasija','tip_na_prikolka','registracija');
@@ -65,6 +66,7 @@ class Spedicija extends CI_Controller{
         $tabela->unset_add();
         $tabela->unset_edit();
         $tabela->unset_delete();
+        $tabela->where('ima_tura','ne');
         $this->prikazi($tabela->render());
     }
     
@@ -89,9 +91,10 @@ class Spedicija extends CI_Controller{
         $tabela->display_as('id_vozilo','Возило');
         $tabela->set_theme('datatables');
         $tabela->set_language('makedonski');
-        $tabela->fields('id_prevoznik','id_spedicija','id_tovar','id_vozilo','id_vozac');
+        $tabela->fields('id_prevoznik','id_spedicija','id_tovar','id_vozilo','id_vozac','zavrsena');
         $tabela->where('id_spedicija',($this->session->userdata('id_korisnik')));
         $tabela->callback_after_insert(array($this, 'smeni_tovar'));
+        $tabela->field_type('zavrsena','hidden','ne');
         //$tabela->callback_add_field('id_spedicija', function () { 
             //$ime=$this->session->userdata('imekompanija');
             //return '<input type="text" maxlength="50" value="'.$ime.'" name="id_spedicija">'; });
