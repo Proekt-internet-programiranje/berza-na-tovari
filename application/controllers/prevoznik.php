d9e22a6f8087fa14f290ebdc85c60fbe06692bb9
stefanstojanov <stefan_stojanov@live.com>
2018-01-31 22:36:48 +0100

    asd
    asd

--- application/controllers/prevoznik.php
+++ application/controllers/prevoznik.php
@@ -29,7 +29,7 @@ class Prevoznik extends CI_Controller{
         $tabela = new grocery_CRUD();
         $tabela->set_table('vozac');
         $tabela->set_relation('id_vozac','korisnici','korisnicko_ime');
-        $tabela->set_relation('id_kompanija','kompanija','imekompanija');
+        $tabela->set_relation('id_kompanija','kompanija','imekompanija',array('id_kompanija' => $id=$this->session->userdata('id_korisnik')));
         $tabela->set_theme('datatables');
         $tabela->set_language('makedonski');
         $tabela->columns('id_vozac','id_kompanija','ime_vozac','tip_na_vozacka');
